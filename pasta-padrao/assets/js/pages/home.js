f1678dac18fd0b52b503621c19404bd4ff0ad164
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-08-17 20:59:21 -0300

    Adiciona uma pasta padrão

---
+++ pasta-padrao/assets/js/pages/home.js
@@ -0,0 +1,11 @@
+// import pages from "aula-02/assets/js";
+//
+// ((fn) => {
+//     if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+//         fn();
+//     } else {
+//         document.addEventListener('DOMContentLoaded', fn);
+//     }
+// })(() => {
+//     pages();
+// });
