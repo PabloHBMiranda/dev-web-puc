8ca52df607dce0ccf9317dfa3aa92de494aaf550
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-08-18 16:19:10 -0300

    Adiciona a verificação das páginas

--- tema-padrao/index.php
+++ tema-padrao/index.php
@@ -1,3 +1,23 @@
 <?php
 include 'inc/layout/templates-header/header-main.php';
+
+
+//use NucleoWeb\Component\URL\URL;
+//
+//$url_args = URL::args();
+//
+//$pagina = $url_args[0] ?? null;
+//
+//if ($pagina == null) $pagina = 'home';
+//
+//// Verifica se a página existe
+//if (file_exists(get_template_directory() . '/pages/' . $pagina . '.php')) {
+//    include 'pages/' . $pagina . '.php';
+//} else {
+//    status_header(404);
+//    $GLOBALS['wp_query']->is_404 = true;
+//
+//    include 'pages/404.php';
+//}
+
 include 'inc/layout/templates-footer/footer-main.php';
