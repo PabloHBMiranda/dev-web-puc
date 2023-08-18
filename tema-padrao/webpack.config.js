63582a4552ba600dae2865c872b3bb91b0300db7
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-08-18 13:49:28 -0300

    Renomeia pastas

--- tema-padrao/webpack.config.js
+++ tema-padrao/webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
     mode: 'production',
     entry: {
         index: [
-            path.resolve(__dirname, 'src', 'index.js'),
-            path.resolve(__dirname, 'src', 'index.scss')
+            path.resolve(__dirname, 'scripts', 'index.js'),
+            path.resolve(__dirname, 'styles', 'index.scss')
         ]
     },
     output: {
