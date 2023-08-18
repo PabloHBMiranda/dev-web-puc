a19fbc6fbd79e7f9f8b33da0b59ce20c58078917
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-08-17 21:43:51 -0300

    Configura o tema padrão

--- aula-02/home.php
+++ aula-02/home.php
@@ -6,11 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bundle/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 <header>
-    <h1 class="titulo">Minha Página Teste</h1>
+    <div class="template-header-main">
+        <h1 class="titulo">Minha Página Teste</h1>
+        <div class="lista-qualquer">
+            <ul class="navbar">
+                <li class="item-list google"><a href="https://www.google.com/" target="_blank">GOOGLE </a></li>
+                <li class="item-list git"><a href="https://github.com/PabloHBMiranda?tab=repositorieS" target="_blank">MEU GIT HUB</a></li>
+                <li class="item-list youtube"><a href="https://www.youtube.com/" target="_blank">YOUTUBE</a></li>
+            </ul>
+            <div class="images">
+                <img src="assets/images/google_capa-3.jpg" alt="" class="image">
+                <img src="assets/images/github.jpg" alt="" class="image">
+                <img src="assets/images/youtube.png" alt="" class="image">
+            </div>
+        </div>
+    </div>
 </header>
 </body>
 </html>
