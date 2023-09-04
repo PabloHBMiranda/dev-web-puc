eaea86c4032b00e2f3a89a88e94fc8201b3f405b
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-09-04 19:30:49 -0300

    Cria nova pastas

---
+++ aula-05/constants.php
@@ -0,0 +1,17 @@
+<?php
+
+//URL do site
+define( 'URL_SITE' , '/dev-web-puc/tema-padrao/');
+
+//Diretório das páginas
+define('PAGES', __DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR);
+
+//Ditetório dos icones
+define('ICONS', __DIR__ . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'icons' . DIRECTORY_SEPARATOR);
+
+//Diretório das imagens
+define('IMAGES', __DIR__ . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR);
+
+//Diterório das fontes
+define('FONTS', __DIR__ . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR);
+
---
+++ aula-05/index.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'constants.php';
+
+$url = $_SERVER["REQUEST_URI"];
+$url_part = explode('/', $url);
+
+$pagina = '';
+
+foreach ($url_part as $key => $value) {
+    if ($value == '') {
+        unset($url_part[$key]);
+    }
+}
+
+if ($url !== null) {
+    if (($url == URL_SITE || strpos($url, 'home') !== false) || count($url_part) <= 2  || end($url_part) == '') {
+        $pagina = 'home.php';
+    } else if(file_exists(PAGES . end($url_part) . '.php')) {
+        $pagina = end($url_part) . '.php';
+    } else{
+        $pagina = '404.php';
+    }
+} else {
+    $pagina = '404.php';
+}
+
+include 'header.php';
+
+if (file_exists(PAGES . $pagina)) {
+    include PAGES . $pagina;
+}
+
+include 'footer.php';
