65caec32e44f6c2cb68b0cceb28e82ded0512196
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-09-04 20:36:18 -0300

    Configura o CSS do header main

--- tema-padrao/templates/sections/header/header-main.php
+++ tema-padrao/templates/sections/header/header-main.php
@@ -1,12 +1,30 @@
+<?php
+
+$items = [
+    'name' => [
+        'Menu 1',
+        'Menu 2',
+        'Menu 3',
+        'Menu 4',
+        'Menu 5',],
+    'url' => ['https://google.com.br',
+        'https://google.com.br',
+        'https://google.com.br',
+        'https://google.com.br',
+        'https://google.com.br',]
+];
+
+?>
+
 <section class="template-header-main">
     <div class="container">
         <div class="wrapper-header-main">
             <ul class="nav-bar">
-                <li class="menu-nav-bar"><a href="#">Menu 1</a></li>
-                <li class="menu-nav-bar"><a href="#">Menu 2</a></li>
-                <li class="menu-nav-bar"><a href="#">Menu 3</a></li>
-                <li class="menu-nav-bar"><a href="#">Menu 4</a></li>
-                <li class="menu-nav-bar"><a href="#">Menu 5</a></li>
+                <?php
+                   for($i = 0; $i < count($items['name']); $i++) {
+                        echo '<li class="menu-nav-bar"><a href="'.$items['url'][$i].'">'.$items['name'][$i].'</a></li>';
+                    }
+                ?>
             </ul>
         </div>
     </div>
