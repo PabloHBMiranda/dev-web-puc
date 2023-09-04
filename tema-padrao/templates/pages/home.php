86d881f226a6e239041993e4886c1d67a7a03b4e
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-09-04 20:53:49 -0300

    Configura o home page padrão

--- tema-padrao/templates/pages/home.php
+++ tema-padrao/templates/pages/home.php
@@ -1,3 +1,9 @@
 <?php
 ?>
-<h1>TESTE</h1>
+<section class="template-home-page">
+    <div class="container">
+        <div class="wrapper-home-page">
+            <p class="text">ESCOLHA UM ITEM DO MENU</p>
+        </div>
+    </div>
+</section>
--- tema-padrao/templates/sections/footer/footer-main.php
+++ tema-padrao/templates/sections/footer/footer-main.php
@@ -1,1 +1,11 @@
-<footer>BBBBBBBBBBBBBBBBB</footer>
+<footer>
+    <section class="template-footer-main">
+        <div class="container">
+            <div class="wrapper-footer-main">
+                <h1 class="footer-main-title">
+                    ©PABLO HENRIQUE B. MIRANDA
+                </h1>
+            </div>
+        </div>
+    </section>
+</footer>
