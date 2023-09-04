45a35047e719974856da418a96e479d8b1ee561b
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-09-04 19:52:48 -0300

    Configura as variaveis do tem

--- tema-padrao/templates/sections/header/header-main.php
+++ tema-padrao/templates/sections/header/header-main.php
@@ -1,3 +1,13 @@
 <section class="template-header-main">
-    <div class="container"></div>
+    <div class="container">
+        <div class="wrapper-header-main">
+            <ul class="nav-bar">
+                <li class="menu-nav-bar"><a href="#">Menu 1</a></li>
+                <li class="menu-nav-bar"><a href="#">Menu 2</a></li>
+                <li class="menu-nav-bar"><a href="#">Menu 3</a></li>
+                <li class="menu-nav-bar"><a href="#">Menu 4</a></li>
+                <li class="menu-nav-bar"><a href="#">Menu 5</a></li>
+            </ul>
+        </div>
+    </div>
 </section>
