6565700c505dc8fc74ce0a29cf9d0d020a428fa3
PabloHBMiranda <pablo.hbmiranda@gmail.com>
2023-09-11 19:08:59 -0300

    Altera o EX-02

--- aula-04/templates/pages/ex-02.php
+++ aula-04/templates/pages/ex-02.php
@@ -3,7 +3,7 @@
     <img src="assets/images/foto-banner-topo.jpg" alt="Menu de Cabeçalho">
 </div>
 <div class="content">
-    <iframe loading="lazy" title="REFORMA ANTES E DEPOIS 2022" src="https://www.youtube.com/embed/-kY1TR8Sc7U?feature=oembed" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/77sUil7ChtM?si=0GNZSQa_ZXTyWsY4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>
 </body>
 </html>
